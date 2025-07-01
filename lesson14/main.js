26f12aae2f14e64d5f5e856a13b57a2e04233c04
vvnec <122540164+vvnec@users.noreply.github.com>
2025-07-01 10:57:00 +0300

    Add files via upload

---
+++ lesson14/main.js
@@ -0,0 +1,34 @@
+const taskInput = document.getElementById('taskInput')
+const addBtn = document.getElementById('addBtn')
+const taskList = document.getElementById('taskList')
+
+
+addBtn.addEventListener('click', addTask)
+
+
+function enter(){
+    if(event.code==='Enter'||event.code==='NumpadEnter') addTask()
+}
+function addTask(){
+    const taskText = taskInput.value.trim();
+    if(taskText ==='') 
+        return;
+
+taskList.innerHTML += `<li><div id="task">${taskText}</div><button onclick="updTask(this.previousElementSibling)" id ="updBtn">Изменить</button><button onclick="this.parentElement.remove()" id="delBtn">Удалить</button></li>`;    
+taskInput.value = '';
+}
+
+function updTask(el){
+    let txt = el.innerHTML
+    if(document.getElementById('updInp')) return
+    el.innerHTML = `<input id="updInp" value="${txt}" onblur="focusOff()">`
+    let updInp = document.getElementById('updInp')
+    updInp.focus()
+}
+function focusOff(){
+     txt = updInp.value
+        updInp.remove()
+        console.log(txt)
+        document.getElementById('task').innerHTML = txt
+}
+
